d5bcf4c94f4b5b1dc9fcdeac9f5a5a206e4183da
Dan Lindsay <dlindsay72@hotmail.com>
2018-01-23 12:10:08 -0500

    basic setup and UI for NoteVC done and project organized

---
+++ Secrets/Controller/NoteVC.swift
@@ -0,0 +1,36 @@
+//
+//  NoteVC.swift
+//  Secrets
+//
+//  Created by Dan Lindsay on 2018-01-23.
+//  Copyright © 2018 Dan Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class NoteVC: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension NoteVC: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
